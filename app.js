006d1829103ee4073f2866f24a8e81d2d9ee1c82
Zeromatiker <zeromatiker@gmail.com>
2015-12-17 22:40:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,13 +12,13 @@ var main = new UI.Menu({
 });
 
 function show(items1, show_update){
-	console.log('show it, items1: ' + items1);
+//	console.log('show it, items1: ' + items1);
 	if (show_update !== true){
 		items1.unshift({title: 'Update', subtitle: 'check for News'});
-		console.log('next: ');
+//		console.log('next: ');
 	}
 	main.items(0, items1);
-	console.log('fertig! geshowt');
+//	console.log('fertig! geshowt');
 }
 
 function request(){
@@ -27,41 +27,29 @@ function request(){
 			type: 'json'
 		},
 		function(data) {
-			return data;
-		},
-		function(error) {
-			console.log('rquest_error');
-			show([{ title: 'No Connection', subtitle: '-> No News' }]);
-		}
-	);
-}
-
-main.show();
-
-var data = request();
-if(data.error !== 0){
+			if(data.error !== 0){
 				show([{title: 'Fehler', subtitle: data.msg }]);
 			} else {
 				var items = [];
 				for(var i = 0; i < data.notifications.length; i++){
-					console.log('run: ' + i);
+//					console.log('run: ' + i);
 					items.push({
 						title: data.notifications[i].subject, 
 						subtitle: data.notifications[i].description
 					});
 				}
-				console.log(items.length + ' items: ' + items);
-				items.push({title: 'Load more', subtitle: 'next page'});
+//				console.log(items.length + ' items: ' + items);
+//				items.push({title: 'Load more', subtitle: 'next page'});
 				show(items);
 			}
 			main.on('select', function(e) {
-				console.log(e.itemIndex);
+//				console.log(e.itemIndex);
 				if (e.itemIndex === 0){
-					console.log('updating');
-					show([{ title: 'Updating...', subtitle: '4 test' }], true);
-//					request();
-				} else if (e.itemIndex == (items.length - 1)){
-					console.log('get new news');
+//					console.log('updating');
+					show([{ title: 'Updating...', subtitle: 'downloading news' }], true);
+					request();
+//				} else if (e.itemIndex == (items.length - 1)){
+//					console.log('get new news');
 				} else if (data.error === 0) {
 					var card = new UI.Card();
 					card.title(items[e.itemIndex].title);
@@ -70,5 +58,14 @@ if(data.error !== 0){
 					card.show();
 				}
 			});
+		},
+		function(error) {
+//			console.log('rquest_error');
+			show([{ title: 'No Connection', subtitle: '-> No News' }]); //add update funciton!
+		}
+	);
+}
+
+main.show();
 
 request();
